064bea109ac56cd2c0a0bb14874130677a8dc7e3
Mukund <137892321+mukund2506@users.noreply.github.com>
2025-08-15 13:42:05 +0530

    UPDATE

--- index.js
+++ index.js
@@ -5,21 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-// Get the button
-const goTopBtn = document.getElementById("goTopBtn");
-
-window.addEventListener("scroll", function () {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        goTopBtn.classList.add("show");
-    } else {
-        goTopBtn.classList.remove("show");
-    }
-});
-
-goTopBtn.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior:"smooth" // Smooth scroll up
-    });
-});
-
