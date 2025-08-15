cd4af966f232feadcfaabe5d5670ea01596794af
Mukund Jagani <137892321+mukund2506@users.noreply.github.com>
2025-08-15 12:18:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+// Get the button
+const goTopBtn = document.getElementById("goTopBtn");
+
+// Show the button when user scrolls down 200px
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        goTopBtn.style.display = "block";
+    } else {
+        goTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to top smoothly when clicked
+goTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
